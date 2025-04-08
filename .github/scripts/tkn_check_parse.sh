2f3e46ea9f8288117e26e162f3d5b58492c492e6
Paul Fitzgerald <pafitzge@redhat.com>
2025-04-08 09:48:37 +0100

    feat(RELEASE-1492): copy_release-service-catalog_linters
    check tknparse
    
    Signed-off-by: Paul Fitzgerald <pafitzge@redhat.com>

--- .github/scripts/tkn_check_parse.sh
+++ .github/scripts/tkn_check_parse.sh
@@ -6,7 +6,7 @@ main() {
   TMPFILE=$(mktemp)
   for file in ${CHANGED_FILES}; do
       echo -en "Checking file ${file}..."
-      tkn bundle push ${REGISTRY} ${file} >/dev/null 2> ${TMPFILE} || true
+      echo $(tkn bundle push  ${REGISTRY} ${file}) #>/dev/null 2> ${TMPFILE} || true
       if ERROR=`grep -o "^Error.*failed to parse.*" ${TMPFILE}`; then
           echo  ${ERROR} && exit 1
       else
